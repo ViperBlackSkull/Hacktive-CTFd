9e2075767669c84ec49f0f473860600c4732ba5f
Viper <vipergrosman@hotmail.com>
2025-03-06 22:27:59 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-ansible-playbook -i "dev.hacktive.education," deploy_ctfd.yml --user=root --key-file /home/vscode/.ssh/kali-snolet
+ansible-playbook -i "dev.hacktive.education," deploy_ctfd.yml --user=root --key-file /home/vscode/.ssh/key
